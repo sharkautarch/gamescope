f8a54aedfd750c7825f9e807f0292eba7069385c
Matthew Anderson <ruinairas1992@gmail.com>
2023-10-06 23:58:17 -0500

    Add initial display selection atom

--- src/xwayland_ctx.hpp
+++ src/xwayland_ctx.hpp
@@ -149,6 +149,7 @@ struct xwayland_ctx_t final : public gamescope::IWaitable
 		Atom gamescopeRotateControl;
 		Atom gamescopeXWaylandModeControl;
 
+		Atom gamescopeConnectorControl;
 		Atom gamescopeFPSLimit;
 		Atom gamescopeDynamicRefresh[DRM_SCREEN_TYPE_COUNT];
 		Atom gamescopeLowLatency;
