924d1af78f75f39bb458cf90b99e6eefe30ab0be
sharkautarch <128002472+sharkautarch@users.noreply.github.com>
2024-01-30 11:07:03 -0500

    allow gamescope to run on devices that only support vulkan 1.2

--- src/rendervulkan.hpp
+++ src/rendervulkan.hpp
@@ -635,7 +635,6 @@ static inline uint32_t div_roundup(uint32_t x, uint32_t y)
 	VK_FUNC(BeginCommandBuffer) \
 	VK_FUNC(BindBufferMemory) \
 	VK_FUNC(BindImageMemory) \
-	VK_FUNC(CmdBeginRendering) \
 	VK_FUNC(CmdBindDescriptorSets) \
 	VK_FUNC(CmdBindPipeline) \
 	VK_FUNC(CmdClearColorImage) \
@@ -643,7 +642,6 @@ static inline uint32_t div_roundup(uint32_t x, uint32_t y)
 	VK_FUNC(CmdCopyImage) \
 	VK_FUNC(CmdDispatch) \
 	VK_FUNC(CmdDraw) \
-	VK_FUNC(CmdEndRendering) \
 	VK_FUNC(CmdPipelineBarrier) \
 	VK_FUNC(CmdPushConstants) \
 	VK_FUNC(CreateBuffer) \
@@ -695,6 +693,10 @@ static inline uint32_t div_roundup(uint32_t x, uint32_t y)
 	VK_FUNC(WaitSemaphores) \
 	VK_FUNC(SetHdrMetadataEXT)
 
+#define VULKAN_1_3_DEVICE_FUNCTIONS \
+	VK_FUNC(CmdBeginRendering) \
+	VK_FUNC(CmdEndRendering)
+	
 template<typename T, typename U = T>
 constexpr T align(T what, U to) {
 return (what + to - 1) & ~(to - 1);
@@ -703,6 +705,7 @@ return (what + to - 1) & ~(to - 1);
 class CVulkanDevice
 {
 public:
+	bool m_supportsReshade = false;
 	bool BInit(VkInstance instance, VkSurfaceKHR surface);
 
 	VkSampler sampler(SamplerState key);
@@ -761,6 +764,7 @@ public:
 	{
 		VULKAN_INSTANCE_FUNCTIONS
 		VULKAN_DEVICE_FUNCTIONS
+		VULKAN_1_3_DEVICE_FUNCTIONS
 	} vk;
 	#undef VK_FUNC
 
@@ -825,6 +829,8 @@ protected:
 	std::atomic<uint64_t> m_submissionSeqNo = { 0 };
 	std::vector<std::unique_ptr<CVulkanCmdBuffer>> m_unusedCmdBufs;
 	std::map<uint64_t, std::unique_ptr<CVulkanCmdBuffer>> m_pendingCmdBufs;
+	
+	uint32_t m_vkApiVer = 0;
 };
 
 struct TextureState
