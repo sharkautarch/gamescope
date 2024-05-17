9c57bc9ea17c8541a2937f763e8674923347db5f
sharkautarch <128002472+sharkautarch@users.noreply.github.com>
2024-05-17 15:24:41 -0400

    add missing constexpr_function class

---
+++ src/constexpr_func.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <memory>
+
+//This constexpr function implementation is based off of
+//a c++23 constexpr function implementation that used c++23 constexpr unique_ptr: https://www.reddit.com/r/cpp/comments/118z87c/c23_constexpr_stdfunction_in_40_loc_simplified/
+template <class>
+class constexpr_function;
+
+template <class R, class... TArgs>
+class constexpr_function<R(TArgs...)> {
+  struct interface {
+    constexpr virtual auto operator()(TArgs...) -> R = 0;
+    constexpr virtual ~interface() = default;
+  };
+
+  template <class Fn>
+  struct implementation final : interface {
+    constexpr implementation(Fn fn) : fn{fn} {}
+    constexpr ~implementation() = default;
+    constexpr auto operator()(TArgs... args) -> R {
+    	if constexpr (std::is_trivial_v<Fn> || std::is_fundamental_v<Fn>)
+     		return;
+     	else if constexpr (sizeof...(TArgs) == 0)
+     		return fn();
+     	else
+     		return fn(args...);
+    }
+
+   private:
+    Fn fn{};
+  };
+
+ public:
+  constexpr ~constexpr_function() {
+    delete fn;
+  }
+  template <class Fn>
+  constexpr constexpr_function(Fn fn) : fn{new implementation<Fn>(fn)} {}
+  constexpr auto operator()(TArgs... args) const -> R {
+     if constexpr (sizeof...(TArgs) == 0)
+     	return (*fn)();
+    return (*fn)(args...);
+  }
+  constexpr operator bool() {
+  	return fn != nullptr;
+  }
+ private: 
+  interface* fn{};
+};
+
+template <class> struct function_traits {};
+
+template <class R, class B, class... TArgs>
+struct function_traits<R (B::*)(TArgs...) const> {
+  using type = R(TArgs...);
+};
+
+template <class F>
+constexpr_function(F) -> constexpr_function<typename function_traits<decltype(&F::operator())>::type>;
